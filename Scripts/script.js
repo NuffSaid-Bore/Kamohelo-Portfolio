8276095e28599a422ae24d0e6834bc676ee81b16
NuffSaid-Bore <borekamohelo@gmail.com>
2024-10-14 16:02:13 +0200

    added context to pages such as about contact and portfolio

--- Scripts/script.js
+++ Scripts/script.js
@@ -4,6 +4,7 @@ const hamburger = document.getElementById("hamburger");
 const menu = document.getElementById("menuList");
 const filterItems = document.querySelectorAll('.filter-item');
 const portfolioItems = document.querySelectorAll('.portfolio-item');
+const scrollers = document.querySelectorAll(".scroller");
 
 
 menuLinks.forEach(link => {
@@ -62,5 +63,29 @@ filterItems.forEach(item => {
     });
 });
 
-
-
+// If a user hasn't opted in for recuded motion, then we add the animation
+if (!window.matchMedia("(prefers-reduced-motion: reduce)").matches) {
+    addAnimation();
+  }
+  
+  function addAnimation() {
+    scrollers.forEach((scroller) => {
+      // add data-animated="true" to every `.scroller` on the page
+      scroller.setAttribute("data-animated", true);
+  
+      // Make an array from the elements within `.scroller-inner`
+      const scrollerInner = scroller.querySelector(".scroller__inner");
+      const scrollerContent = Array.from(scrollerInner.children);
+  
+      // For each item in the array, clone it
+      // add aria-hidden to it
+      // add it into the `.scroller-inner`
+      scrollerContent.forEach((item) => {
+        const duplicatedItem = item.cloneNode(true);
+        duplicatedItem.setAttribute("aria-hidden", true);
+        scrollerInner.appendChild(duplicatedItem);
+      });
+    });
+  }
+  
+ 
